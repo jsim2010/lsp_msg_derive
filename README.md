1b3358fe5c697ef7d309017eeebb789b78a2744a
Justin Simerly <justinsimerly1992@gmail.com>
2019-04-18 11:08:33 -0400

    Adding eval and fix cargo cmds

---
+++ README.md
@@ -0,0 +1,7 @@
+# lsp_msg_derive
+
+Implements macros for `lsp_msg` crate.
+
+These macros remove much of the repetition from the LSP definitions.
+
+License: MIT
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
+//! Implements macros for `lsp_msg` crate.
+//!
+//! These macros remove much of the repetition from the LSP definitions.
 extern crate proc_macro;
 
-use proc_macro::{TokenTree, TokenStream};
+use proc_macro::{Literal, TokenStream, TokenTree};
 use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::quote;
-use syn::{Ident, Fields, parse_macro_input, Item};
+use syn::{parse_macro_input, Fields, Ident, Item, Type};
 
+/// Holds the state of the attribute parser.
 enum AttributeParserState {
+    /// Ready to parse next attribute.
     Option,
+    /// Ready to parse value assigned to dynamic_registration option.
     DynamicRegistrationValue,
+    /// Ready to parse value assigned to link_support option.
     LinkSupportValue,
+    /// Ready to parse value assigned to markup_kind_list option.
     MarkupKindListValue,
+    /// Ready to parse value assigned to triggers option.
     TriggersValue,
+    /// Ready to parse value assigned to resolve_provider option.
     ResolveProviderValue,
 }
 
 impl AttributeParserState {
-    fn is_searching_for_value(&self) -> bool {
+    /// If parser is currently at a valid end.
+    fn is_complete(&self) -> bool {
         match self {
             AttributeParserState::Option => false,
             _ => true,
@@ -23,116 +34,120 @@ impl AttributeParserState {
     }
 }
 
-#[proc_macro_attribute]
-pub fn lsp_object(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mut allow_missing_attr = quote!{};
-    let mut dynamic_registration = None;
-    let mut link_support = None;
-    let mut markup_kind_list = None;
-    let mut triggers = None;
-    let mut resolve_provider = None;
-    let mut has_document_selector = false;
-    let mut has_static_registration = false;
+/// Defines attributes of the `lsp_object` attribute.
+#[derive(Default)]
+struct Attributes {
+    /// Sets struct when deserializing to set any missing field to its default.
+    pub(crate) allow_missing_attr: TokenStream2,
+    /// Includes dynamic registration parameter with given documentation variable.
+    pub(crate) dynamic_registration: Option<Literal>,
+    /// Includes link support parameter with given documentation variable.
+    pub(crate) link_support: Option<Literal>,
+    /// Includes markup kind parameter with given documentation variable.
+    pub(crate) markup_kind_list: Option<Literal>,
+    /// Includes resolve provider parameter with given documentation variable.
+    pub(crate) triggers: Option<Literal>,
+    /// Includes resolve provider parameter with given documentation variable.
+    pub(crate) resolve_provider: Option<Literal>,
+    /// Includes text document registration parameter(s).
+    pub(crate) has_document_selector: bool,
+    /// Includes static registration parameter(s).
+    pub(crate) has_static_registration: bool,
+}
+
+/// Retrieves the `Attributes` provided to `lsp_object` attribute.
+fn get_attributes(attr: TokenStream) -> Attributes {
     let mut state = AttributeParserState::Option;
+    let mut attributes = Attributes::default();
 
     for token in attr {
         match state {
             AttributeParserState::Option => {
-                match token {
-                    TokenTree::Ident(ident) => {
-                        match ident.to_string().as_str() {
-                            "allow_missing" => {
-                                allow_missing_attr = quote!{
-                                    #[serde(default)]
-                                };
-                            }
-                            "document_selector" => {
-                                has_document_selector = true;
-                            }
-                            "static_registration" => {
-                                has_static_registration = true;
-                            }
-                            "dynamic_registration" => {
-                                state = AttributeParserState::DynamicRegistrationValue;
-                            }
-                            "link_support" => {
-                                state = AttributeParserState::LinkSupportValue;
-                            }
-                            "markup_kind_list" => {
-                                state = AttributeParserState::MarkupKindListValue;
-                            }
-                            "triggers" => {
-                                state = AttributeParserState::TriggersValue;
-                            }
-                            "resolve_provider" => {
-                                state = AttributeParserState::ResolveProviderValue;
-                            }
-                            option => {
-                                panic!("Unsupported attribute option: {}", option);
-                            }
+                if let TokenTree::Ident(ident) = token {
+                    match ident.to_string().as_str() {
+                        "allow_missing" => {
+                            attributes.allow_missing_attr = quote! {
+                                #[serde(default)]
+                            };
+                        }
+                        "document_selector" => {
+                            attributes.has_document_selector = true;
+                        }
+                        "static_registration" => {
+                            attributes.has_static_registration = true;
+                        }
+                        "dynamic_registration" => {
+                            state = AttributeParserState::DynamicRegistrationValue;
+                        }
+                        "link_support" => {
+                            state = AttributeParserState::LinkSupportValue;
+                        }
+                        "markup_kind_list" => {
+                            state = AttributeParserState::MarkupKindListValue;
+                        }
+                        "triggers" => {
+                            state = AttributeParserState::TriggersValue;
+                        }
+                        "resolve_provider" => {
+                            state = AttributeParserState::ResolveProviderValue;
+                        }
+                        option => {
+                            panic!("Unsupported attribute option: {}", option);
                         }
                     }
-                    _ => (),
                 }
             }
             AttributeParserState::DynamicRegistrationValue => {
-                match token {
-                    TokenTree::Literal(literal) => {
-                        dynamic_registration = Some(literal);
-                        state = AttributeParserState::Option;
-                    }
-                    _ => (),
+                if let TokenTree::Literal(literal) = token {
+                    attributes.dynamic_registration = Some(literal);
+                    state = AttributeParserState::Option;
                 }
             }
             AttributeParserState::LinkSupportValue => {
-                match token {
-                    TokenTree::Literal(literal) => {
-                        link_support = Some(literal);
-                        state = AttributeParserState::Option;
-                    }
-                    _ => (),
+                if let TokenTree::Literal(literal) = token {
+                    attributes.link_support = Some(literal);
+                    state = AttributeParserState::Option;
                 }
             }
             AttributeParserState::MarkupKindListValue => {
-                match token {
-                    TokenTree::Literal(literal) => {
-                        markup_kind_list = Some(literal);
-                        state = AttributeParserState::Option;
-                    }
-                    _ => (),
+                if let TokenTree::Literal(literal) = token {
+                    attributes.markup_kind_list = Some(literal);
+                    state = AttributeParserState::Option;
                 }
             }
             AttributeParserState::TriggersValue => {
-                match token {
-                    TokenTree::Literal(literal) => {
-                        triggers = Some(literal);
-                        state = AttributeParserState::Option;
-                    }
-                    _ => (),
+                if let TokenTree::Literal(literal) = token {
+                    attributes.triggers = Some(literal);
+                    state = AttributeParserState::Option;
                 }
             }
             AttributeParserState::ResolveProviderValue => {
-                match token {
-                    TokenTree::Literal(literal) => {
-                        resolve_provider = Some(literal);
-                        state = AttributeParserState::Option;
-                    }
-                    _ => (),
+                if let TokenTree::Literal(literal) = token {
+                    attributes.resolve_provider = Some(literal);
+                    state = AttributeParserState::Option;
                 }
             }
         }
     }
 
-    if state.is_searching_for_value() {
+    if state.is_complete() {
         panic!("Missing a value for an option.");
     }
 
+    attributes
+}
+
+/// An attribute-like macro to define LSP objects from structs.
+#[proc_macro_attribute]
+#[inline]
+pub fn lsp_object(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let attributes = get_attributes(attr);
     let input = if let Item::Struct(item_struct) = parse_macro_input!(item as Item) {
         item_struct
     } else {
         panic!("Error");
     };
-    
+
     let name = input.ident;
     let vis = input.vis;
     let generics = input.generics;
@@ -145,8 +160,8 @@ pub fn lsp_object(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let mut fields: Vec<TokenStream2> = Vec::new();
 
-    if has_document_selector {
-        fields.push(quote!{
+    if attributes.has_document_selector {
+        fields.push(quote! {
             /// Identifies the scope of the registration.
             ///
             /// If `Option::None`, `DocumentSelector` provided by client will be used.
@@ -154,65 +169,68 @@ pub fn lsp_object(attr: TokenStream, item: TokenStream) -> TokenStream {
         });
     }
 
-    if has_static_registration {
-        fields.push(quote!{
+    if attributes.has_static_registration {
+        fields.push(quote! {
             /// The id used to register the request.
             id: Elective<String>
         });
     }
 
-    if let Some(doc_var) = dynamic_registration {
+    if let Some(doc_var) = attributes.dynamic_registration {
         let mut d = doc_var.to_string();
         d.retain(|c| c != '"');
         let doc = format!("Supports dynamic registration of the {}.", d);
 
-        fields.push(quote!{
+        fields.push(quote! {
             #[doc = #doc]
             dynamic_registration: bool
         });
     }
 
-    if let Some(doc_var) = link_support {
+    if let Some(doc_var) = attributes.link_support {
         let mut d = doc_var.to_string();
         d.retain(|c| c != '"');
         let doc = format!("Supports additional metadata in the form of {} links.", d);
 
-        fields.push(quote!{
+        fields.push(quote! {
             #[doc = #doc]
             link_support: bool
         });
     }
 
-    if let Some(property) = markup_kind_list {
+    if let Some(property) = attributes.markup_kind_list {
         let mut p = property.to_string();
         p.retain(|c| c != '"');
         let doc = format!("The supported `MarkupKind`s for the `{}` property.\n\nThe order describes the preferred format.", p);
         let property_name = format!("{}_format", p);
         let name = Ident::new(&property_name, Span::call_site());
 
-        fields.push(quote!{
+        fields.push(quote! {
             #[doc = #doc]
             #name: Vec<MarkupKind>
         });
     }
 
-    if let Some(doc_var) = triggers {
+    if let Some(doc_var) = attributes.triggers {
         let mut d = doc_var.to_string();
         d.retain(|c| c != '"');
         let doc = format!("Characters that trigger {} automatically.", d);
 
-        fields.push(quote!{
+        fields.push(quote! {
             #[doc = #doc]
             trigger_characters: Vec<String>
         });
     }
 
-    if let Some(doc_var) = resolve_provider {
+    if let Some(doc_var) = attributes.resolve_provider {
         let mut d = doc_var.to_string();
         d.retain(|c| c != '"');
-        let doc = format!("Provides support to resolve additional information for a {} item.", d);
+        let doc = format!(
+            "Provides support to resolve additional information for a {} item.",
+            d
+        );
 
-        fields.push(quote!{
+        fields.push(quote! {
             #[doc = #doc]
             resolve_provider: bool
         });
@@ -220,35 +238,35 @@ pub fn lsp_object(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     for field in old_fields {
         let mut is_elective = false;
-        match &field.ty {
-            syn::Type::Path(p) => {
-                if let Some(segment) = p.path.segments.first() {
-                    if segment.value().ident.to_string() == String::from("Elective") {
-                        is_elective = true;
-                    }
+
+        if let Type::Path(ref p) = field.ty {
+            if let Some(segment) = p.path.segments.first() {
+                if segment.value().ident == "Elective" {
+                    is_elective = true;
                 }
             }
-            _ => (),
-        };
+        }
         let elective_attr = if is_elective {
-            quote!{
+            quote! {
                 #[serde(default, skip_serializing_if = "Elective::is_absent")]
             }
         } else {
-            quote!{}
+            quote! {}
         };
         let field_type = field.ty;
         let field_name = field.ident;
         let field_attrs = field.attrs;
         let field_vis = field.vis;
-        fields.push(quote!{
+        fields.push(quote! {
             #elective_attr
             #(#field_attrs)*
             #field_vis #field_name: #field_type
         });
     }
 
-    let output = quote!{
+    let allow_missing_attr = attributes.allow_missing_attr;
+
+    let output = quote! {
         #[derive(Debug, Default, Deserialize, Serialize)]
         #[serde(rename_all = "camelCase")]
         #allow_missing_attr
@@ -261,30 +279,22 @@ pub fn lsp_object(attr: TokenStream, item: TokenStream) -> TokenStream {
     TokenStream::from(output)
 }
 
+/// An attribute-like macro to define LSP kinds from enums.
 #[proc_macro_attribute]
+#[inline]
 pub fn lsp_kind(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mut kind_attrs = quote!{
+    let mut kind_attrs = quote! {
         #[derive(Debug, Deserialize, Serialize)]
         #[serde(rename_all = "camelCase")]
     };
 
     for token in attr {
-        match token {
-            TokenTree::Ident(ident) => {
-                match ident.to_string().as_str() {
-                    "number" => {
-                        kind_attrs = quote!{
-                            #[derive(Debug, Deserialize_repr, Serialize_repr)]
-                            #[repr(u8)]
-                        };
-                    }
-                    _ => {
-                        panic!("Error parsing lsp_kind");
-                    }
-                }
-            }
-            _ => {
-                panic!("Error parsing lsp_kind");
+        if let TokenTree::Ident(ident) = token {
+            if ident.to_string().as_str() == "number" {
+                kind_attrs = quote! {
+                    #[derive(Debug, Deserialize_repr, Serialize_repr)]
+                    #[repr(u8)]
+                };
             }
         }
     }
@@ -295,7 +305,7 @@ pub fn lsp_kind(attr: TokenStream, item: TokenStream) -> TokenStream {
         panic!("Error");
     };
 
-    let output = quote!{
+    let output = quote! {
         #kind_attrs
         #input
     };
